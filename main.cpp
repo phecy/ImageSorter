fcc73a3532ac3c2d67c3962b69f136eb0b4e4909
Uliana <uliana@soe.ucsc.edu>
2011-01-14 11:22:26 -0800

    Corrected spacing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-This file is part of the ImageSorter.
+    This file is part of the ImageSorter.
 
     ImageSorter is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
