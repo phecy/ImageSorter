6668fcf1f6d4f996df2e5d7c43fca4624412c2a6
Armin Samii [Ubuntu] <ksamii@ucsc.edu>
2011-03-05 01:37:11 -0800

    The php file for parsing data

---
+++ results/duplicatedataparser.php
@@ -0,0 +1,139 @@
+<?
+$data = 
+array(array(array("new1-DSC_0031.JPG","1","1","1","1","1"), array("new1-DSC_0075.JPG","1","1","1","1","1"), array("new1-DSC_0099.JPG","1","1","1","1","1"), array("new1-DSC_0190.JPG","1","1","1","1","1")),
+array(array("new1-DSC_0037.JPG","1","1","1","1","1"), array("new1-DSC_0078.JPG","1","1","1","1","1"), array("new1-DSC_0102.JPG","1","1","1","1","1"), array("new1-DSC_0193.JPG","1","1","1","1","1")),
+array(array("new1-DSC_0034.JPG","1","1","1","1","1"), array("new1-DSC_0082.JPG","2","1","1","2","2"), array("new1-DSC_0105.JPG","1","1","1","1","1"), array("new1-DSC_0198.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0039.JPG","1","1","1","1","1"), array("new1-DSC_0085.JPG","2","1","1","2","2"), array("new1-DSC_0109.JPG","1","1","1","1","1"), array("new1-DSC_0201.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0035.JPG","1","1","1","1","1"), array("new1-DSC_0090.JPG","1","1","2","1","3"), array("new1-DSC_0114.JPG","2","2","2","2","2"), array("new1-DSC_0209.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0046.JPG","1","1","1","1","1"), array("new1-DSC_0094.JPG","1","1","2","2","3"), array("new1-DSC_0117.JPG","3","3","1","2","3"), array("new1-DSC_0218.JPG","3","3","3","1","3")),
+array(array("new1-DSC_0048.JPG","2","2","2","2","2"), array("new1-DSC_0098.JPG","1","1","2","2","3"), array("new1-DSC_0121.JPG","3","3","1","2","3"), array("new1-DSC_0221.JPG","3","3","3","1","3")),
+array(array("new1-DSC_0056.JPG","2","2","2","2","2"), array("new1-DSC_0076.JPG","1","1","1","2","1"), array("new1-DSC_0100.JPG","1","1","1","1","1"), array("new1-DSC_0191.JPG","1","1","1","1","1")),
+array(array("new1-DSC_0074.JPG","2","5","2","3","5"), array("new1-DSC_0079.JPG","1","1","1","1","1"), array("new1-DSC_0103.JPG","1","1","1","1","1"), array("new1-DSC_0196.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0049.JPG","2","2","2","2","2"), array("new1-DSC_0083.JPG","2","1","1","2","2"), array("new1-DSC_0106.JPG","1","1","1","1","1"), array("new1-DSC_0199.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0059.JPG","2","2","2","2","2"), array("new1-DSC_0086.JPG","2","1","1","2","2"), array("new1-DSC_0110.JPG","3","3","1","2","3"), array("new1-DSC_0202.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0072.JPG","2","5","2","3","5"), array("new1-DSC_0096.JPG","1","1","2","1","3"), array("new1-DSC_0119.JPG","3","3","1","2","3"), array("new1-DSC_0219.JPG","3","3","3","1","3")),
+array(array("new1-DSC_0075.JPG","2","5","2","3","5"), array("new1-DSC_0099.JPG","1","1","2","1","3"), array("new1-DSC_0122.JPG","3","3","1","2","3"), array("new1-DSC_0223.JPG","3","3","3","1","3")),
+array(array("new1-DSC_0055.JPG","2","2","2","2","2"), array("new1-DSC_0077.JPG","1","1","1","1","1"), array("new1-DSC_0101.JPG","1","1","1","1","1"), array("new1-DSC_0192.JPG","1","1","1","1","1")),
+array(array("new1-DSC_0060.JPG","2","2","2","2","2"), array("new1-DSC_0080.JPG","1","1","1","2","4"), array("new1-DSC_0104.JPG","1","1","1","1","1"), array("new1-DSC_0197.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0067.JPG","2","2","2","2","2"), array("new1-DSC_0084.JPG","2","1","1","1","2"), array("new1-DSC_0107.JPG","1","1","1","1","1"), array("new1-DSC_0200.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0073.JPG","2","5","2","3","5"), array("new1-DSC_0087.JPG","1","1","1","2","5"), array("new1-DSC_0111.JPG","3","3","1","2","3"), array("new1-DSC_0205.JPG","2","2","2","1","2")),
+array(array("new1-DSC_0062.JPG","3","3","3","4","3"), array("new1-DSC_0093.JPG","1","1","2","2","3"), array("new1-DSC_0116.JPG","2","2","2","2","2"), array("new1-DSC_0216.JPG","3","3","3","1","3")),
+array(array("new1-DSC_0069.JPG","4","4","3","4","4"), array("new1-DSC_0097.JPG","1","1","2","5","3"), array("new1-DSC_0120.JPG","3","3","1","2","3"), array("new1-DSC_0220.JPG","3","3","3","1","3")));
+
+$appoutput = array(
+array("new1-DSC_0031.JPG", -1,5,5,7,6,2,2,1,0,0,0,0,0,0,0,0,0,0,0,0),
+array("new1-DSC_0034.JPG", 5,-1,10,5,5,1,2,2,1,0,0,0,0,0,0,0,0,0,0,0),
+array("new1-DSC_0035.JPG", 5,10,-1,6,5,2,2,2,1,0,0,0,0,0,0,0,0,0,0,0),
+array("new1-DSC_0037.JPG", 7,5,6,-1,9,2,2,2,2,1,0,0,0,0,0,0,0,0,0,0),
+array("new1-DSC_0039.JPG", 6,5,5,9,-1,1,2,2,2,2,1,1,0,0,0,0,0,0,0,0),
+array("new1-DSC_0046.JPG", 2,1,2,2,1,-1,4,4,1,1,3,3,2,1,0,0,0,0,0,0),
+array("new1-DSC_0048.JPG", 2,2,2,2,2,4,-1,10,1,2,1,1,5,2,2,2,0,0,0,0),
+array("new1-DSC_0049.JPG", 1,2,2,2,2,4,10,-1,1,3,2,2,4,2,2,2,1,0,0,0),
+array("new1-DSC_0055.JPG", 0,1,1,2,2,1,1,1,-1,2,2,1,1,1,3,2,2,2,1,1),
+array("new1-DSC_0056.JPG", 0,0,0,1,2,1,2,3,2,-1,1,2,3,3,1,2,1,2,2,2),
+array("new1-DSC_0059.JPG", 0,0,0,0,1,3,1,2,2,1,-1,9,2,3,1,2,1,4,2,2),
+array("new1-DSC_0060.JPG", 0,0,0,0,1,3,1,2,1,2,9,-1,2,3,1,2,1,3,2,2),
+array("new1-DSC_0062.JPG", 0,0,0,0,0,2,5,4,1,3,2,2,-1,3,2,3,2,2,4,4),
+array("new1-DSC_0064.JPG", 0,0,0,0,0,1,2,2,1,3,3,3,3,-1,1,3,2,4,2,3),
+array("new1-DSC_0067.JPG", 0,0,0,0,0,0,2,2,3,1,1,1,2,1,-1,2,2,2,1,1),
+array("new1-DSC_0069.JPG", 0,0,0,0,0,0,2,2,2,2,2,2,3,3,2,-1,1,3,3,2),
+array("new1-DSC_0072.JPG", 0,0,0,0,0,0,0,1,2,1,1,1,2,2,2,1,-1,10,1,2),
+array("new1-DSC_0073.JPG", 0,0,0,0,0,0,0,0,2,2,4,3,2,4,2,3,10,-1,2,4),
+array("new1-DSC_0074.JPG", 0,0,0,0,0,0,0,0,1,2,2,2,4,2,1,3,1,2,-1,3),
+array("new1-DSC_0075.JPG", 0,0,0,0,0,0,0,0,1,2,2,2,4,3,1,2,2,4,3,-1)
+);
+
+// Print for debugging
+print("Printing turk data...i\n\n<br><br>");
+for($set=0; $set<4; ++$set) {
+   print("Printing set {$set}\n<br>");
+   for($user=1; $user<=5; ++$user) {
+      for($row=0; $row < 19; ++$row) {
+         print($data[$row][$set][$user] . ", ");
+      }
+      print("\n<br>");
+   }
+   print("\n<br>\n<br>\n<br>\n<br>");
+}
+
+// Get results array
+$results;
+for($set=0; $set<4; ++$set) {
+   for($user=1; $user<=5; ++$user) {
+      for($row=0; $row < 19; ++$row) {
+         $thisrowgroup = $data[$row][$set][$user];
+         for($rowi=0; $rowi < 19; ++$rowi) {
+            $filename = $data[$row][$set][0];
+            $filenamei = $data[$rowi][$set][0];
+
+            if($rowi != $row)
+               if($data[$row][$set][$user] == $data[$rowi][$set][$user])
+                  $results[$set][$filename][$filenamei]++;
+         }
+      }
+      print("\n<br>");
+   }
+   print("\n<br>\n<br>\n<br>\n<br>");
+}
+
+// Print results
+for($set=0; $set<4; ++$set) {
+   print("Printing set {$set}\n<br>");
+   print("{");
+   for($rowi=0; $rowi < 19; ++$rowi) {
+      $filenamei = $data[$rowi][$set][0];
+      print("{");
+      for($row=0; $row < 19; ++$row) {
+         $filename = $data[$row][$set][0];
+
+         $result = $results[$set][$filename][$filenamei];
+         if($result == "") $result = "0";
+         print($result);
+
+         if($row != 18)
+            print(", ");
+      }
+      print("},");
+      print("\n<br>");
+   }
+   print("}");
+   print("\n<br>\n<br>\n<br>\n<br>");
+}
+
+
+// Compare to our data
+print("Printing our data...\n\n<br><br>");
+for($row=0; $row<=19; ++$row) {
+   for($rowi=1; $rowi < 19; ++$rowi) {
+      print($appoutput[$row][$rowi] . ", ");
+   }
+   print("\n<br>");
+}
+
+
+// TO DO: The data isn't even right. GROUPS vote, not images. dafuck.
+print("Printing our success?\n\n<br><br>");
+$ourthreshold = 6; // Out of 10
+$turkthreshold = 4; // Out of 5 users
+$set = 0; // Set number 0
+$total = 0; // Total pairs
+$success = 0; // Num successes
+for($row=0; $row<=19; ++$row) {
+   $filename = $appoutput[$row][0];
+   for($rowi=$row; $rowi < 19; ++$rowi) {
+      if($row == $rowi) continue;
+      $filenamei = $appoutput[$rowi][0];
+      $turkvote = $results[$set][$filename][$filenamei] >= $turkthreshold;
+      $ourvote = $appoutput[$row][$rowi] >= $ourthreshold;
+      if($ourvote == $turkvote) {
+         print("One success!\n<br>");
+         ++$success;
+      } else {
+         print("...fail\n<br>");
+      }
+      ++$total;
+   }
+   print("\n<br>");
+}
+print("Final result...{$success} right out of {$total} pairs!\n<br>");
+?>
