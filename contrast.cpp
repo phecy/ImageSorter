72e4a0bab926db6b971edb2ea42ce79210267747
Armin [Lexus] <ksamii@ucsc.edu>
2011-05-07 12:41:13 -0700

    Added summed area tables for speed

--- contrast.cpp
+++ contrast.cpp
@@ -16,7 +16,7 @@ float Contrast::RMS(VImage *vim)
 
     double sumOfSquareDiffs = 0;
 
-    for(int j=0; j<hist.size(); ++j) {
+    for(unsigned int j=0; j<hist.size(); ++j) {
         double diff = hist[j]*(j - avgGray);
         sumOfSquareDiffs += diff*diff;
     }
@@ -27,29 +27,49 @@ float Contrast::RMS(VImage *vim)
     return rms;
 }
 
-
-int Contrast::local_mid_grey(VImage *vim, int width, int height, int x, int y)
-{
+sumMap* Contrast::makeGraySumMap(VImage* vim) {
     QImage* qimage = vim->getQImage();
-    int grey = 0;
-    int cnt = 0;
+    int width = vim->getWidth();
+    int height = vim->getHeight();
 
-    // Make histogram
-    for(int h=max(y-height/2, 0);   h<min(height/2+y, qimage->height()); ++h) {
-        for(int w=max(x-width/2,0); w<min(width/2+x,qimage->width());    ++w) {
-            QRgb color = qimage->pixel(w, h);
-            grey += qGray(color);
-            cnt++;
+    sumMap* densityMap = new sumMap();
+
+    for(int y=0; y<height; ++y) {
+        vector<unsigned long int> row(width);
+        densityMap->push_back(row);
+        for(int x=0; x<width; ++x) {
+            unsigned long int currSum =
+                  qGray(qimage->pixel(x, y));
+            if(y > 0) {
+                currSum +=
+                  (*densityMap)[y][max(x-1, 0)]
+                + (*densityMap)[max(y-1, 0)][x]
+                - (*densityMap)[max(y-1, 0)][max(x-1, 0)];
+            }
+
+            (*densityMap)[y][x] = currSum;
         }
     }
-    grey = grey/cnt;
-    return grey;
+
+    return densityMap;
 }
 
-int Contrast::derivative(VImage *vim, int x, int y)
+int Contrast::local_mid_grey(sumMap* graySumMap, int width, int height, int x, int y)
 {
+    int starty = max(y-height/2, 0);
+    int endy   = min(y+height/2, (int)graySumMap->size());
+    int startx = max(x-width/2, 0);
+    int endx   = min(x+width/2, (int)graySumMap[0].size());
 
-    int w=0;
+    int area = (1+endy-starty) * (1+endx-startx);
+
+    unsigned long totalGray =
+         (*graySumMap)[endy][endx]
+       + (*graySumMap)[starty][startx]
+       - (*graySumMap)[starty][endx]
+       - (*graySumMap)[endy][startx];
+
+    return totalGray / (area+1);
 }
 
 
@@ -59,21 +79,22 @@ float Contrast::local_contrast(VImage *vim)
     int area = qimage->width() * qimage->height();
     int diff = 0;
 
-    // Make histogram
-    for(int h=0; h<qimage->height(); ++h) {
-        for(int w=0; w<qimage->width(); ++w) {
-            QRgb color = qimage->pixel(w, h);
-            int grey = qGray(color);
-            int local_grey = local_mid_grey(vim, local_width, local_height, w, h);
-            diff += abs(grey - local_grey);
+    sumMap* graySumMap = makeGraySumMap(vim);
 
-       //     printf("grey %d    local_mid %d      diff %d\n", grey, local_grey, diff);
+    // Make histogram
+    for(int h=0; h<qimage->height() - local_height; ++h) {
+        for(int w=0; w<qimage->width() - local_width; ++w) {
+            int grey = qGray(qimage->pixel(w, h));
+            int local_grey = local_mid_grey(graySumMap, local_width, local_height, w, h);
+            diff += abs(grey - local_grey);
         }
 
     }
 
 
     printf("local contrast = %d \n", diff);
+
+    delete graySumMap;
 
     return diff/area;
 
--- contrast.h
+++ contrast.h
@@ -3,14 +3,16 @@
 
 #include "vimage.h"
 
+typedef vector<vector<unsigned long int> > sumMap;
+
 class Contrast
 {
 public:
     Contrast();
     float RMS(VImage *vim);
     float local_contrast(VImage *vim);
-    int local_mid_grey(VImage *vim, int width, int height, int x, int y);
-    int derivative(VImage *vim, int x, int y);
+    sumMap* makeGraySumMap(VImage* vim);
+    int local_mid_grey(sumMap*, int width, int height, int x, int y);
 };
 
 #endif // CONTRAST_H
